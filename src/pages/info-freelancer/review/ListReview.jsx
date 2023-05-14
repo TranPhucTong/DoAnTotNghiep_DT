49fa2fc6574f6f94d865a5cb096b8c4bdc830825
danh <thdanh2001@gmail.com>
2023-05-14 13:18:28 +0700

    .

--- src/pages/info-freelancer/review/ListReview.jsx
+++ src/pages/info-freelancer/review/ListReview.jsx
@@ -1,29 +1,21 @@
-import React from 'react'
-import ItemReview from './ItemReview'
-import noReview from '../../../assests/imgs/no-review.png'
-const ListReview = ({reviews}) => {
+import React from "react";
+import ItemReview from "./ItemReview";
+import noReview from "../../../assests/imgs/no-review.png";
+const ListReview = ({ reviews }) => {
   return (
-    <div className='h-full'>
-        
-        {reviews.length ?
-         reviews.map((review) => <ItemReview key ={review._id} review = {review}/>)
-         :
-         <div>
-            <h3 className='text-[#6C63FF] font-bold text-center text-xl'>Chưa có đánh giá nào</h3>
-            <img src={noReview} alt="img" />
-         </div>
-        }
-        {reviews.length ?
-         reviews.map((review) => <ItemReview key ={review._id} review = {review}/>)
-         :
-         <div>
-            <h3 className='text-[#6C63FF] font-bold text-center text-xl'>Chưa có đánh giá nào</h3>
-            <img src={noReview} alt="img" />
-         </div>
-        }
-         
+    <div className="h-full">
+      {reviews.length ? (
+        reviews.map((review) => <ItemReview key={review._id} review={review} />)
+      ) : (
+        <div>
+          <h3 className="text-[#6C63FF] font-bold text-center text-xl">
+            Chưa có đánh giá nào
+          </h3>
+          <img src={noReview} alt="img" />
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default ListReview
+export default ListReview;
